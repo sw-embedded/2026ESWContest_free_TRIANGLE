93dfeaeeff18fe1e8f62fb4a4ee47e9d99514140
Lim chaeyeon <20242622@sungshin.ac.kr>
2026-08-31 11:29:25 +0900

    Update status.py

--- src/monitor/status.py
+++ src/monitor/status.py
@@ -1,6 +1,6 @@
 # src/monitor/status.py 내용 예시
 class PoseController:
-    """실시간 자세 및 모니터링 상태 관리 클래스"""
+    ""실시간 자세 및 모니터링 상태 관리 클래스""
     def __init__(self):
         self.current_status = {
             "pose": "INIT",
